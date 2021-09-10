29b1638b115e3c987b99c1f30963b3bafb6cdd20
Jonathan Wright <j.wright72127@gmail.com>
2021-09-10 10:56:00 -0600

    Fisrt push

---
+++ Keepr.client/src/env.js
@@ -0,0 +1,5 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'https://localhost:5001' : ''
+export const domain = 'codeworks-jonathan.us.auth0.com'
+export const audience = 'https://mainApi.com'
+export const clientId = 'DLuOHJT5ppl6RF22KJWztSrmtYe1BjSe'
